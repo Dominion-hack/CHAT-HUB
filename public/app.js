580778fa65f809a016e1f59ed41877303e17fe83
Dominion-hack <helenjoyikpe@gmail.com>
2026-05-18 11:13:57 +0300

    Delete app.js

--- public/app.js
+++
@@ -1,8 +0,0 @@
-function sendMessage(){
-  const msg = document.getElementById("msg").value;
-  const box = document.getElementById("chat-box");
-  const p = document.createElement("p");
-  p.textContent = localStorage.getItem("username") + ": " + msg;
-  box.appendChild(p);
-  document.getElementById("msg").value = "";
-}
